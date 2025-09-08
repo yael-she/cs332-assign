3deef635fd2d7491635b160a2e8ef408c10248f7
yael-she <yaelisheinfeld@gmail.com>
2025-09-08 16:31:54 +0900

    fix pascal func

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -2,7 +2,7 @@ package recfun
 import common._
 
 object Main {
-  def main(args: Array[String]) {
+  def main(args: Array[String]): Unit = {
     println("Pascal's Triangle")
     for (row <- 0 to 10) {
       for (col <- 0 to row)
@@ -15,18 +15,19 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0 || r == 0) 1
-    else pascal(c - 1, r - 1) + pascal(c - 1, r)
+    require {c >= 0 && r >= 0 && c <= r}
+    if (c == 0 || r == c) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
   /**
    * Exercise 2
    */
-  def balance(chars: List[Char]): Boolean = ???
+//  def balance(chars: List[Char]): Boolean = ???
 
   /**
    * Exercise 3
    */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+//  def countChange(money: Int, coins: List[Int]): Int = ???
 
 }
